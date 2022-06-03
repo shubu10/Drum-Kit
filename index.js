0ea7c9db8256ed4f97510767be0e6e8782c0665a
Shubu <shubu.d10@gmail.com>
2022-06-04 03:07:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", handleClick);
+}
+
+
+function handleClick() {
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+}
+
+
+// ***********************************************************
+
+
+
+document.addEventListener("keydown", (e) => {
+    makeSound(e.key);
+    buttonAnimation(e.key);
+})
+
+
+function makeSound(key) {
+
+    if (key === "w") {
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+    }
+    else if (key === "a") {
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+    }
+    else if (key === "s") {
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+    }
+    else if (key === "d") {
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+    }
+    else if (key === "j") {
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+    }
+    else if (key === "k") {
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+    }
+    else if (key === "l") {
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+    }
+
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(()=>{
+        activeButton.classList.remove("pressed");
+    },100)
+}
